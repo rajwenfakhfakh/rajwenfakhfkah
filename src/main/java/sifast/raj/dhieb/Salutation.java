2adec25014476f0eb38b59437378c4de3fab3050
asus <asus@DESKTOP-KH59FB5>
2017-01-04 14:23:14 +0100

    Salutation nouveau projet Maven

---
+++ src/main/java/sifast/raj/dhieb/Salutation.java
@@ -0,0 +1,14 @@
+package sifast.raj.dhieb;
+
+public class Salutation {
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi there , Have a nice day ");
+		
+	}
+
+}
